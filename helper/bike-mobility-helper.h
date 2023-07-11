d98ab10ea37610fc665f17f8f2c91253e4bf8a68
moheed-ali <moheedalikayani@outlook.com>
2023-07-11 12:11:38 +0200

    Add bike application, mobility-helper and testing example

---
+++ helper/bike-mobility-helper.h
@@ -0,0 +1,43 @@
+#ifndef BIKE_MOBILITY_HELPER_H
+#define BIKE_MOBILITY_HELPER_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+#include "ns3/core-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+
+using namespace ns3;
+
+struct BikeData {
+    std::string bikeNumber;
+    int duration;
+    long time_started; // now it is sec
+    long time_ended; // now it is sec
+    int start_station;
+    int end_station;
+    double start_lat;
+    double start_lng;
+    double end_lat;
+    double end_lng;
+};
+
+std::vector<BikeData> readDataset(const std::string& filename);
+
+void PrintNodePosition(ns3::Ptr<ns3::Node> node);
+
+std::map<std::string, int> createBikeNumberMap(const std::vector<BikeData>& dataset);
+
+ns3::Ptr<ns3::WaypointMobilityModel> SaveWaypoints(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap, ns3::NodeContainer nodes);
+
+std::map<long, int> start_time_of_node(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap);
+
+std::map<long, int> end_time_of_node(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap);
+
+#endif  // BIKE_MOBILITY_HELPER_H
