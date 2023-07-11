38c809f378d1ff32e72056b5213eb2da8005bfed
moheed-ali <moheedalikayani@outlook.com>
2023-07-11 12:15:27 +0200

    Custom header file for bike mobility

---
+++ model/app/bike-application.h
@@ -0,0 +1,40 @@
+
+
+#ifndef BIKE_APPLICATION_H
+#define BIKE_APPLICATION_H
+
+#include "ns3/lora-application.h"
+
+namespace ns3
+{
+namespace lorawan
+{
+
+class BikeApplication : public Application
+{
+  public:
+    static TypeId GetTypeId(void);
+
+    BikeApplication();
+    virtual ~BikeApplication();
+
+    void SetNode(Ptr<Node> node);  // New function to set the node
+    void PrintNodePosition();      // New function to print node position
+
+//   protected:
+//     void DoInitialize() override;
+//     void DoDispose() override;
+
+  private:
+    virtual void StartApplication();
+    virtual void StopApplication();
+    void ScheduleNextPositionPrint();  // New function to schedule next position print
+
+    Ptr<Node> m_node;      // Node pointer
+    EventId m_printEvent;  // EventId for the periodic printing
+};
+
+} // namespace lorawan
+
+} // namespace ns3
+#endif /* SENDER_APPLICATION */
