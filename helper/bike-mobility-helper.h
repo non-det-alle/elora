191d55925f20e6ee9d954f48d211e956eac86697
moheed-ali <moheedalikayani@outlook.com>
2023-07-12 16:55:46 +0200

    Class is Implemented

--- helper/bike-mobility-helper.h
+++ helper/bike-mobility-helper.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <string>
 
 #include "ns3/core-module.h"
 #include "ns3/mobility-module.h"
@@ -13,7 +14,8 @@
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
 
-using namespace ns3;
+namespace ns3
+{
 
 struct BikeData {
     std::string bikeNumber;
@@ -28,16 +30,62 @@ struct BikeData {
     double end_lng;
 };
 
-std::vector<BikeData> readDataset(const std::string& filename);
+class BikeHelper{
+    public: 
 
-void PrintNodePosition(ns3::Ptr<ns3::Node> node);
+        BikeHelper();
+        ~BikeHelper();
 
-std::map<std::string, int> createBikeNumberMap(const std::vector<BikeData>& dataset);
+        ns3::NodeContainer create_nodes_helper();
+        int Get_Num_of_Nodes();
 
-ns3::Ptr<ns3::WaypointMobilityModel> SaveWaypoints(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap, ns3::NodeContainer nodes);
+        void SetFileName(const std::string& filename);
 
-std::map<long, int> start_time_of_node(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap);
+        void PrintNodePosition(ns3::Ptr<ns3::Node> node);
 
-std::map<long, int> end_time_of_node(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap);
+        std::map<long, int> start_time_of_node(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap);
 
-#endif  // BIKE_MOBILITY_HELPER_H
+        std::map<long, int> end_time_of_node(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap);
+    private:
+        std::string filename;
+        std::vector<BikeData> dataset;
+        std::map<std::string, int> myMap;
+        ns3::NodeContainer nodes;
+
+        std::vector<BikeData> readDataset(const std::string& filename);
+        ns3::Ptr<ns3::WaypointMobilityModel> SaveWaypoints(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap, ns3::NodeContainer nodes);
+        std::map<std::string, int> createBikeNumberMap(const std::vector<BikeData>& dataset);
+};
+
+
+
+} // namespace ns3
+#endif /* BIKE_HELPER_H  */
+
+
+// struct BikeData {
+//     std::string bikeNumber;
+//     int duration;
+//     long time_started; // now it is sec
+//     long time_ended; // now it is sec
+//     int start_station;
+//     int end_station;
+//     double start_lat;
+//     double start_lng;
+//     double end_lat;
+//     double end_lng;
+// };
+
+// std::vector<BikeData> readDataset(const std::string& filename);
+
+// void PrintNodePosition(ns3::Ptr<ns3::Node> node);
+
+// std::map<std::string, int> createBikeNumberMap(const std::vector<BikeData>& dataset);
+
+// ns3::Ptr<ns3::WaypointMobilityModel> SaveWaypoints(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap, ns3::NodeContainer nodes);
+
+// std::map<long, int> start_time_of_node(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap);
+
+// std::map<long, int> end_time_of_node(const std::vector<BikeData>& dataset, const std::map<std::string, int> myMap);
+
+
