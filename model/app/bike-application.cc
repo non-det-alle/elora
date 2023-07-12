7af8c7e4a37acfa99ed1e7a5ef8d0725842ce74f
moheed-ali <moheedalikayani@outlook.com>
2023-07-12 14:02:51 +0200

    Changing the print position functionality with send packets

--- model/app/bike-application.cc
+++ model/app/bike-application.cc
@@ -1,6 +1,8 @@
 
 
 #include "bike-application.h"
+#include "ns3/mobility-module.h"
+#include "ns3/simulator.h"
 
 
 namespace ns3
@@ -16,13 +18,13 @@ TypeId
 BikeApplication::GetTypeId(void)
 {
     static TypeId tid = TypeId("ns3::BikeApplication")
-                            .SetParent<Application>()
+                            .SetParent<LoraApplication>()
                             .AddConstructor<BikeApplication>()
                             .SetGroupName("lorawan");
     return tid;
 }
 
-BikeApplication::BikeApplication() : m_node(nullptr), m_printEvent() 
+BikeApplication::BikeApplication()
 {
     NS_LOG_FUNCTION(this);
 }
@@ -32,77 +34,45 @@ BikeApplication::~BikeApplication()
     NS_LOG_FUNCTION(this);
 }
 
-void 
-BikeApplication::SetNode(Ptr<Node> node) {
-    m_node = node;
+void
+BikeApplication::StartApplication(void)
+{
+    NS_LOG_FUNCTION(this);
+    // Schedule the next SendPacket event
+    Simulator::Cancel(m_sendEvent);
+    NS_LOG_DEBUG("Starting up application with a first event with a " << m_initialDelay.GetSeconds()
+                                                                      << " seconds delay");
+    m_sendEvent = Simulator::Schedule(m_initialDelay, &BikeApplication::SendPacket, this);
+    NS_LOG_DEBUG("Event Id: " << m_sendEvent.GetUid());
 }
 
-void 
-BikeApplication::PrintNodePosition() {
-    if (m_node) {
-        Ptr<WaypointMobilityModel> waypointMobility = m_node->GetObject<WaypointMobilityModel>();
-        NS_LOG_INFO("NODE ID:  " << m_node->GetId() << " | Node position: " << waypointMobility->GetPosition() << " , Time is : " << Simulator::Now().GetSeconds());
-        // std :: cout << "NODE ID:  " << m_node->GetId() << " | Node position: " << waypointMobility->GetPosition() << " , Time is : " << Simulator::Now().GetSeconds() << std :: endl;
-        // std::cout << "Waypoint Left = " << waypointMobility->WaypointsLeft() << std :: endl;
-        // Get the next waypoint time in sec
-        // double nextWaypointInSeconds = waypointMobility->GetNextWaypoint().time.GetSeconds();
-        // std::cout << "Next Waypoint arrival Time = " << nextWaypointInSeconds << " , Time is : " << Simulator::Now().GetSeconds() << std :: endl;
-        std :: cout<<"_____________________________________________________" << std :: endl;
-
-        ScheduleNextPositionPrint();
-    }
-}
-
-void 
-BikeApplication::ScheduleNextPositionPrint() {
-    Ptr<WaypointMobilityModel> waypointMobility = m_node->GetObject<WaypointMobilityModel>();
-    if(waypointMobility->WaypointsLeft() == 0 && waypointMobility->GetVelocity() == Vector3D(0, 0, 0)){
-        Simulator::Cancel(m_printEvent);
-    }
-    else {
-         if(waypointMobility->WaypointsLeft() %2 != 0){
-            std::cout << "Waypoint Left = " << waypointMobility->WaypointsLeft() << std :: endl; 
-            double time = Simulator::Now().GetSeconds();   
-            double nWInSec = waypointMobility->GetNextWaypoint().time.GetSeconds();
-            m_printEvent = Simulator::Schedule(Seconds(nWInSec - time), &BikeApplication::PrintNodePosition, this);
-        }
-        else{
-            std::cout << "Waypoint Left = " << waypointMobility->WaypointsLeft() << std :: endl;    
-                           //60 * 60 * 24
-            m_printEvent = Simulator::Schedule(Seconds(1), &BikeApplication::PrintNodePosition, this);
-        }     
-    }
-}
-
-// void
-// BikeApplication::DoInitialize()
-// {
-//     NS_LOG_FUNCTION(this);
+void
+BikeApplication::SendPacket(void)
+{
+    NS_LOG_FUNCTION(this);
     
-//     //LoraApplication::DoInitialize();
-// }
+    auto waypointMobility = m_node->GetObject<WaypointMobilityModel>();
+    NS_ASSERT(bool(waypointMobility) != 0);
+    NS_LOG_DEBUG("NODE ID:  " << m_node->GetId() << " | Node position: " << waypointMobility->GetPosition() << " , Time is : " << Simulator::Now().GetSeconds());
+    if(waypointMobility->WaypointsLeft() == 0 && waypointMobility->GetVelocity() == Vector3D(0, 0, 0)){
+        Simulator::Cancel(m_sendEvent);
+        return;
+    }
 
-// void
-// BikeApplication::DoDispose()
-// {
-//     NS_LOG_FUNCTION(this);
-//     m_node = nullptr;
-//     //LoraApplication::DoDispose();
-// }
+    NS_LOG_DEBUG("Waypoint Left = " << waypointMobility->WaypointsLeft());
+    if(waypointMobility->WaypointsLeft() %2 != 0){
+        Time now = Simulator::Now();   
+        Time next = waypointMobility->GetNextWaypoint().time; // next > now is ensured by WaypointMobilityModel
+        m_sendEvent = Simulator::Schedule(next - now, &BikeApplication::SendPacket, this);
+        return;
+    }
 
-
-void 
-BikeApplication::StartApplication() {
-    NS_LOG_INFO("BikeApplication::StartApplication called for node = "  << m_node->GetId());
-    //std :: cout << "BikeApplication::StartApplication called for node = "  << m_node->GetId() << std :: endl;
-    ScheduleNextPositionPrint();  // Schedule the first position print
-}
-
-void 
-BikeApplication::StopApplication() {
-    NS_LOG_INFO("BikeApplication::StopApplication called for node = " << m_node->GetId());
-    //std :: cout << "BikeApplication::StopApplication called for node = " << m_node->GetId() << std :: endl; 
-    Simulator::Cancel(m_printEvent);  // Cancel the position print event
+    // Create and send a new packet
+    Ptr<Packet> packet = Create<Packet>(m_basePktSize);
+    //m_mac->Send(packet);
+    // Schedule the next SendPacket event
+    m_sendEvent = Simulator::Schedule(m_avgInterval, &BikeApplication::SendPacket, this);
+    NS_LOG_DEBUG("Sent a packet of size " << packet->GetSize());
 }
 
 } // namespace lorawan
