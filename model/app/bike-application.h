f2f66f7b7cbfd4039a3ba31612c4eb13e95a6813
moheed-ali <moheedalikayani@outlook.com>
2023-07-12 14:02:41 +0200

    Changing the print position functionality with send packets

--- model/app/bike-application.h
+++ model/app/bike-application.h
@@ -4,39 +4,32 @@
 #define BIKE_APPLICATION_H
 
 #include "ns3/lora-application.h"
-#include "ns3/mobility-module.h"
-#include "ns3/simulator.h"
 
 namespace ns3
 {
 namespace lorawan
 {
 
-class BikeApplication : public Application
+class BikeApplication : public LoraApplication
 {
   public:
-    static TypeId GetTypeId(void);
-
     BikeApplication();
     virtual ~BikeApplication();
 
-    void SetNode(Ptr<Node> node);  // New function to set the node
-    void PrintNodePosition();      // New function to print node position
+    static TypeId GetTypeId(void);
 
 //   protected:
 //     void DoInitialize() override;
 //     void DoDispose() override;
 
   private:
-    virtual void StartApplication();
-    virtual void StopApplication();
-    void ScheduleNextPositionPrint();  // New function to schedule next position print
-
-    Ptr<Node> m_node;      // Node pointer
-    EventId m_printEvent;  // EventId for the periodic printing
+    virtual void StartApplication(void);
+    //virtual void StopApplication(void);
+    
+    void SendPacket(void);  // New function to schedule next position print
 };
 
 } // namespace lorawan
 
 } // namespace ns3
-#endif /* BIKE_APPLICATION  */
+#endif /* BIKE_APPLICATION_H  */
